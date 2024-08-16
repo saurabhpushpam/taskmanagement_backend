020749aa9fd5c16ecf8663aafcab6589ad36f51d
saurabhpushpam <145956198+saurabhpushpam@users.noreply.github.com>
2024-08-17 03:29:21 +0530

    commit

--- index.js
+++ index.js
@@ -10,25 +10,25 @@ app.use(express.json());
 
 const mongoose = require('mongoose');
 
-// const DB = "mongodb+srv://spuspam111:Sp123456@cluster0.0taaaup.mongodb.net/furniro?retryWrites=true&w=majority";
-// mongoose.connect(DB)
-//     .then(() => {
-//         console.log("Connected to MongoDB");
-//         const server = http.createServer(app);
-//         server.listen(PORT, () => {
-//             console.log(`Server is running on :${PORT}`);
-//         });
-//     })
-//     .catch(error => {
-//         console.error("Error connecting to MongoDB:", error);
-//     });
+const DB = "mongodb+srv://spuspam111:Sp123456@cluster0.0taaaup.mongodb.net/furniro?retryWrites=true&w=majority";
+mongoose.connect(DB)
+    .then(() => {
+        console.log("Connected to MongoDB");
+        const server = http.createServer(app);
+        server.listen(PORT, () => {
+            console.log(`Server is running on :${PORT}`);
+        });
+    })
+    .catch(error => {
+        console.error("Error connecting to MongoDB:", error);
+    });
 
 
-const conn = mongoose.connect("mongodb://127.0.0.1:27017/taskmanagement").then(() => {
-    console.log('MongoDB Connected');
-}).catch((err) => {
-    console.error('MongoDB connection error:', err);
-});
+// const conn = mongoose.connect("mongodb://127.0.0.1:27017/taskmanagement").then(() => {
+//     console.log('MongoDB Connected');
+// }).catch((err) => {
+//     console.error('MongoDB connection error:', err);
+// });
 
 
 // user routes
@@ -45,7 +45,7 @@ app.use('/api', task_route);
 const PORT = 5000;
 
 
-app.listen(PORT, function () {
-    console.log('server is running on port : ', PORT);
-});
+// app.listen(PORT, function () {
+//     console.log('server is running on port : ', PORT);
+// });
 
